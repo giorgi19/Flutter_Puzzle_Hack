afdd3973bff438908aca9862eec76c4a4207833a
unknown <nadiradze19@gmail.com>
2022-01-08 19:56:37 +0400

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Puzzle());
+}
+
+class Puzzle extends StatelessWidget {
+  const Puzzle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
